da57b568476c0d642af77e2f262855e48fd8b4b2
Vadym Mariiechko <vadimich348@gmail.com>
2020-02-29 16:53:53 +0100

    Allow label to be clicked

---
+++ clicklabel.py
@@ -0,0 +1,18 @@
+from PyQt5.QtCore import *
+
+
+def clickable(widget):
+    class Filter(QObject):
+        clicked = pyqtSignal()
+
+        def eventFilter(self, obj, event):
+            if obj == widget:
+                if event.type() == QEvent.MouseButtonRelease:
+                    if obj.rect().contains(event.pos()):
+                        self.clicked.emit()
+                        return True
+            return False
+
+    filter = Filter(widget)
+    widget.installEventFilter(filter)
+    return filter.clicked
