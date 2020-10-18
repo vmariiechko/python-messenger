9243359bcf93945ee3dc1288558ccaa4c587eb98
marik348 <vadimich348@gmail.com>
2020-10-18 14:40:12 +0200

    Rebuild content structure

--- client_content.py
+++ client_content.py
@@ -1,78 +1,86 @@
 def getWarningMessages():
-    return {"emptyStr": '<html><head/><body><p><br/></p></body></html>',
-            "registered": '<html><head/><body><p><span style=" font-style:italic; color:#ef2929;">Username '
-                          'is already registered</span></p></body></html> ',
-            "loginRequired": '<html><head/><body><p><span style=" font-style:italic; color:#ef2929;">Login is '
-                             'required</span></p></body></html>',
-            "invalidLogin": '<html><head/><body><p><span style=" font-style:italic; color:#ef2929;">Username '
-                            'doesn\'t exist</span></p></body></html> ',
-            "loginOutOfRange": '<html><head/><body><p><span style=" font-style:italic; color:#ef2929;">Username '
-                               'must be between 4 and 20 in length</span></p></body></html> ',
-            "passwordRequired": '<html><head/><body><p><span style=" font-style:italic; color:#ef2929;">Password is '
-                                'required</span></p></body></html> ',
-            "invalidPassword": '<html><head/><body><p><span style=" font-style:italic; color:#ef2929;">Password '
-                               'doesn\'t match</span></p></body></html> ',
-            "passwordOutOfRange": '<html><head/><body><p><span style=" font-style:italic; color:#ef2929;">Password '
-                                  'must be between 4 and 20 in length</span></p></body></html> ',
-            "notAlphanumeric": '<html><head/><body><p><span style=" font-style:italic; color:#ef2929;">Login can '
-                               'only contain alphanumeric characters</span></p></body></html>',
-            "banned": '<html><head/><body><p><span style=" font-style:italic; color:#ef2929;">Account '
-                      'was banned</span></p></body></html>',
-            }
+    html_begin = '<span style=" font-style:italic; color:#ef2929;">'
+    html_end = '</span>'
+
+    warning_messages = {
+        "emptyStr": '',
+        "registered": 'Username is already registered',
+        "loginRequired": 'Login is required',
+        "invalidLogin": 'Username doesn\'t exist',
+        "loginOutOfRange": 'Username must be between 4 and 20 in length',
+        "passwordRequired": 'Password is required',
+        "invalidPassword": 'Password doesn\'t match',
+        "passwordOutOfRange": 'Password must be between 4 and 20 in length',
+        "notAlphanumeric": 'Login can only contain alphanumeric characters',
+        "banned": 'Account was banned',
+    }
+
+    warning_messages = {key: html_begin + value + html_end for key, value in warning_messages.items()}
+
+    return warning_messages
 
 
 def getClientCommands():
     return [
-            {'name': 'close', 'description': 'Close the messenger',
+            {'name': 'close',
+             'description': 'Close the messenger',
              'detailed': '<b>Usage:</b> /close<br>'
                          'Ask you to close messenger.'},
-            {'name': 'logout', 'description': 'Logout account',
+            {'name': 'logout',
+             'description': 'Logout account',
              'detailed': '<b>Usage:</b> /logout<br>'
                          'Ask you to logout account.'},
-            {'name': 'reload', 'description': 'Clear commands messages',
+            {'name': 'reload',
+             'description': 'Clear commands messages',
              'detailed': '<b>Usage:</b> /reload<br>'
                          'Clear all commands` messages.'},
             ]
 
 
 def getMessageBoxText():
-    return {"close": "<span style=\"font-size: 15px\">Are you sure to quit?</span>",
-            "logout": "<span style=\"font-size: 15px\">Are you sure to logout?</span>",
-            "about": "<span style=\"font-size: 15px\">"
-                         "Messenger **.**.2020<br>"
-                         "Version: 1.1"
-                     "</span><br><br>"
-                         "Created by Vadym Mariiechko",
-            "contacts": "<span style=\"font-size: 15px\">"
-                            "Vadym Mariiechko:<br>"
-                            "LinkedIn: <a href='http<h5>s://www.linkedin.com/in/mariiechko/'>mariiechko</a><br>"
-                            "GitHub: <a href='https://github.com/marik348'>marik348</a>"
-                        "</span> ",
-            "serverIsOff": "<span style=\"font-size: 15px\">The server is offline</span>",
-            "shortcuts": "<span style=\"font-size: 14px\"><table>"
-                            "<tr>"
-                                "<th>Shortcuts&nbsp;&nbsp;</th>"
-                                "<th>Description</th>"
-                            "</tr>"
-                            "<tr>"
-                                "<td><code>Cntrl+A</code></td>"
-                                "<td>Account settings</td>"
-                            "</tr>"
-                            "<tr>"
-                                "<td><code>Cntrl+R</code></td>"
-                                "<td>Open preferences</td>"
-                            "</tr>"
-                            "<tr>"
-                                "<td><code>Cntrl+S</code></td>"
-                                "<td>Show available shortcuts</td>"
-                            "</tr>"
-                            "<tr>"
-                                "<td><code>Cntrl+D</code></td>"
-                                "<td>Show available commands</td>"
-                            "</tr>"
-                            "<tr>"
-                                "<td><code>Enter</code></td>"
-                                "<td>Send message</td>"
-                            "</tr>"
-                        "</table></span>"
-            }
+    html_begin = '<span style="font-size: 15px">'
+    html_end = '</span>'
+
+    shortcuts_table = "<table>" \
+                            "<tr>" \
+                                "<th>Shortcuts&nbsp;&nbsp;</th>" \
+                                "<th>Description</th>" \
+                            "</tr>" \
+                            "<tr>" \
+                                "<td><code>Cntrl+S</code></td>" \
+                                "<td>Show available shortcuts</td>" \
+                            "</tr>" \
+                            "<tr>" \
+                                "<td><code>Cntrl+D</code></td>" \
+                                "<td>Show available commands</td>" \
+                            "</tr>" \
+                            "<tr>" \
+                                "<td><code>Cntrl+G</code></td>" \
+                                "<td>Logout from accout</td>" \
+                            "</tr>" \
+                            "<tr>" \
+                                "<td><code>Alt+E</code></td>" \
+                                "<td>Close the messenger</td>" \
+                            "</tr>" \
+                            "<tr>" \
+                                "<td><code>Enter</code></td>" \
+                                "<td>Send message</td>" \
+                            "</tr>" \
+                        "</table>"
+
+    message_box_text = {"close": "Are you sure to quit?",
+                        "logout": "Are you sure to logout?",
+                        "about": "Messenger **.**.2020<br>"
+                                 "Version: 1.2"
+                                 "<br><br>",
+                        "contacts": "Vadym Mariiechko:<br><br>"
+                                    "LinkedIn: <a href='http<h5>s://www.linkedin.com/in/mariiechko/'>mariiechko</a><br>"
+                                    "GitHub: <a href='https://github.com/marik348'>marik348</a>",
+                        "serverIsOff": "The server is offline",
+                        "shortcuts": shortcuts_table
+                        }
+
+    message_box_text = {key: html_begin + value + html_end for key, value in message_box_text.items()}
+    message_box_text["about"] += "Created by Vadym Mariiechko"
+
+    return message_box_text
