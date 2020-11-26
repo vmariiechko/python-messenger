73d68d9d89abce446630752fad1d40d99431225a
marik348 <vadimich348@gmail.com>
2020-11-26 19:29:36 +0100

    Add MANIFEST.in

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/marik348/python-messegner",
     packages=find_packages(),
+    include_package_data=True,
     classifiers=[
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3.8",
